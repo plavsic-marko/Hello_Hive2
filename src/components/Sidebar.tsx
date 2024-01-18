f3fd746fbc046ad3a6e43b3fb604536da2b9197d
marko <plavsicmarko10@gmail.com>
2024-01-18 01:54:24 +0100

    useState open-close sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,28 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import "../components/Sidebar.css";
-import { Typography } from "@mui/material";
+import { IconButton, Typography } from "@mui/material";
 import DensityMediumIcon from "@mui/icons-material/DensityMedium";
 import sidebarData from "../data/SidebarData";
+import { grey } from "@mui/material/colors";
 
 const Sidebar = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const handleOpenMenu = () => setOpenMenu(!openMenu);
+
   return (
-    <div className="sidebar">
+    <div className={openMenu ? "sidebar" : "sidebar_close"}>
       <div className="bodyContainer">
         <div className="headerContainer">
-          <DensityMediumIcon />
-          <Typography variant="h5">hello resume</Typography>
+          <IconButton onClick={handleOpenMenu}>
+            <DensityMediumIcon></DensityMediumIcon>
+          </IconButton>
+          <Typography variant="h5" color={grey[100]} marginTop={0.5}>
+            {openMenu ? "helloresume" : "hr"}
+          </Typography>
         </div>
-        <div className="profileContainer">
+        <div
+          className={openMenu ? "profileContainer" : "profileContainer_close"}
+        >
+          <img
+            className={openMenu ? "profile_pic" : "profile_pic_close"}
+            src="public\profilna.jpg"
+            alt="avatar"
+          ></img>
           <div className="profileContents">
-            <Typography variant="body1">Marko Plavsic</Typography>
+            <Typography variant="body1">
+              {openMenu ? "Marko Plavsic" : "MP"}
+            </Typography>
           </div>
         </div>
         <div>
           <ul className="contentContainer">
             {sidebarData.map((item) => (
               <li className="sidebar-item" key={item.id}>
-                {item.icon}
-                <span className="item_title">{item.title}</span>
+                <a className="a_sidebar" href={item.link}>
+                  {item.icon}
+                  <span className="item_title">
+                    {openMenu ? item.title : null}
+                  </span>
+                </a>
+                <div
+                  className="on_hover"
+                  style={openMenu ? { display: "none" } : { display: "flex" }}
+                >
+                  {openMenu ? null : item.title}
+                </div>
               </li>
             ))}
           </ul>
--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -1,6 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./HomePage";
 
-const router = createBrowserRouter([{ path: "/", element: <HomePage /> }]);
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/", element: <HomePage /> },
+  { path: "/", element: <HomePage /> },
+  { path: "/", element: <HomePage /> },
+]);
 
 export default router;
